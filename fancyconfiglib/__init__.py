f6c87794848db2a5cb29e88629ee096fe2b3ca0b
Patric-k-k <104700188+Patric-k-k@users.noreply.github.com>
2025-03-24 19:36:42 -0400

    Added save_data() feature

--- fancyconfiglib/__init__.py
+++ fancyconfiglib/__init__.py
@@ -1,4 +1,4 @@
-# .fconfig File Parser v1.1
+# .fconfig File Parser v1.2
 # Created by Patrick G. Karhoff [Github: Patric-k-k]
 
 def to_data_type(data):
@@ -66,3 +66,46 @@ def read_data(key,file,die_on_error = True):
                         else:
                             return None
     raise KeyError # Couldn't find the key
+def save_data(key,file,data,die_on_error=True):
+    with open(file, 'r') as f:
+        Fdata = f.readlines()
+    for i in Fdata:
+        Processed = i.split("|")
+        if Processed[0] == key:
+            try:
+                Full_data_section = Processed[len(Processed)-1]
+                comment = Full_data_section.split("<")[0]
+                if len(Full_data_section)-1 == 1:
+                    raise Exception("Invalid config file (No data)")
+                if len(Processed)-1 == 1:
+                    Fdata[Fdata.index(i)] = f"{Processed[0]}|{comment}<{data}>"
+                elif len(Processed)-1 == 2:
+                    Fdata[Fdata.index(i)] = f"{Processed[0]}|{Processed[1]}|{comment}<{data}>"
+                else:
+                    if die_on_error == True:
+                        raise Exception(f"Invalid config file (len: {len(Processed)})")
+                    else:
+                        return Exception(f"Invalid config file (len: {len(Processed)})")
+            except Exception as err:
+                if die_on_error == True:
+                    raise err
+                else:
+                    return err
+    #make SURE that lines get seperated.
+    for i in Fdata:
+        Fdata[Fdata.index(i)] = Fdata[Fdata.index(i)] + "\n"
+    #make lists work
+    for i in Fdata:
+        if len(i.split("|")) > 1:
+            Key = i.split("|")[0]
+            Dtype = i.split("|")[1].lower()
+            if i.split("|")[1].lower() == "list" or i.split("|")[1].lower() == "klist":
+                e = i.split("|")[2].replace("'","").replace("[","").replace("]","")
+                Fdata[Fdata.index(i)] = f"{Key}|{Dtype}|{e}"
+    #remove blank lines
+    for i in Fdata:
+        if Fdata[Fdata.index(i)] == "\n" or Fdata[Fdata.index(i)] == "\n\n":
+            Fdata.remove(i)
+    print(Fdata)
+    with open(file, 'w+') as f:
+        f.writelines(Fdata)
